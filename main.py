bab2de4b83e855275c6a4721da68514f4c087d86
lucasbmiotto <lucas.b.miotto@hotmail.com>
2023-03-08 18:44:56 -0300

    Testando a Lógica

--- main.py
+++ main.py
@@ -14,3 +14,11 @@ def cifra_cesar(texto, chave, modo):
             resultado += c
     
     return resultado
+
+# Teste de Cifra
+texto_original = "Hello"
+chave = 3
+texto_encriptado = cifra_cesar(texto_original, chave, "encriptar")
+print("Texto encriptado:", texto_encriptado)
+texto_desencriptado = cifra_cesar(texto_encriptado, chave, "descriptar")
+print("Texto desencriptado:", texto_desencriptado)
