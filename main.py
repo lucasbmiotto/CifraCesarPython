f877b580a859f3a1fd1464d8fd201472fbb6a640
lucasbmiotto <lucas.b.miotto@hotmail.com>
2023-03-08 18:39:51 -0300

    Lógica da cifra

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-# Cifra de Cesar
+# Definindo Chave, texto , descriptar e incriptar 
+def cifra_cesar(texto, chave, modo):
+    resultado = ""
+    
+    for c in texto:
+        if c.isalpha():
+            c = c.upper()
+            if modo == "encriptar":
+                deslocamento = (ord(c) - 65 + chave) % 26
+            else:
+                deslocamento = (ord(c) - 65 - chave) % 26
+            resultado += chr(deslocamento + 65)
+        else:
+            resultado += c
+    
+    return resultado
